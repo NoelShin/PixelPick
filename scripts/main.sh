182ecc9d3f3441f339c92dc1e8add00d3bd5948f
Gyungin Shin <gyungin@robots.ox.ac.uk>
2021-04-01 21:10:57 +0900

    Delete main.sh

--- scripts/main.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-python3 ../main_al.py --dataset_name 'voc' --network_name 'deeplab' --n_pixels_by_us 10 --seed 0 -qs "margin_sampling"
-python3 ../main_al.py --dataset_name 'cs' --network_name 'deeplab' --n_pixels_by_us 10 --seed 0 -qs "margin_sampling"
-python3 ../main_al.py --dataset_name 'cv' --network_name 'deeplab' --n_pixels_by_us 10 --seed 0 -qs "margin_sampling"
-
